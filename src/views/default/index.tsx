a6cec0ffbf7c3078c48ebd3feb06ffc9c57cd3c8
Erik Peña <erikpena@users.noreply.github.com>
2020-11-04 21:16:27 -0800

    Updated sample sets - Updated sample sets with more relevant examples - Used obfuscated id values - Made ids between samples consistent for ease to follow along - Updated default view with id info for dev's edification

--- src/views/default/index.tsx
+++ src/views/default/index.tsx
@@ -19,6 +19,17 @@ const DefaultView = () => {
     <H1Styled>What is this?</H1Styled>
     <p>The Webhook Payload Explorer is a way to view example webhook payloads for uploads that would be sent from Mux to better understand the properties that will be available.</p>
     <p>The menu represents a grouping of webhook events based on the common <strong>use-case</strong>.  Each use-case section header briefly describes the use-case itself as well as some key points that should be considered for the events.  The set of events underneath the use-case header are numbered based on sequence for which they are sent out.</p>
+    <p>These example payloads will reference ids (that don't actually exist).  Refer to the following table to get an idea of what id values to expect in the samples—</p>
+    <dl>
+      <dt><b>Asset Id</b></dt>
+      <dd><code>fy3vqn7C02DKdtEAs8wFfXO01GX00Y666zrjiDNDU1PaiQ</code></dd>
+      <dt><b>Playback Id</b></dt>
+      <dd><code>bWPykzuurdv00Ry4jEJaPNhiek7666et2RoYg02IbOYBc</code></dd>
+      <dt><b>Environment Id</b></dt>
+      <dd><code>bg3p21</code></dd>
+      <dt><b>Direct Upload Id</b></dt>
+      <dd><code>MksbBSugGGHtHvP01YEiKl2sfs701xhBibUHrwRCJgSXo</code></dd>
+    </dl>
   </ContainerStyled>
 }
 
